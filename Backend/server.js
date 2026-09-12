c547c8c39a876a81afdd6cf20434648df60ae389
sawera <shaikhnaqeeb368@gmail.com>
2026-09-12 17:21:11 +0500

    update

--- Backend/server.js
+++ Backend/server.js
@@ -25,12 +25,12 @@ app.get("/", (req, res) => {
     res.send("LearnHub API is running");
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 mongoose
 .connect(process.env.MONGO_URI)
 .then (() => {
     console.log("MongoDB Atlas connected");
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    // app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 })
 .catch((err) => console.error("MongoDB connection error:", err.message));
