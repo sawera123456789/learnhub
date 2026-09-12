9549d3d68420a4e615f85ec98ca425d8326e1ea9
sawera123456789 <shaikhnaqeeb368@gmail.com>
2026-09-12 18:07:56 +0500

    Update API URL for courses data

--- Frontend/my-project/src/pages/Courses.jsx
+++ Frontend/my-project/src/pages/Courses.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import CourseCard from "../components/CourseCard";
 
-const API_URL = "http://localhost:5000/api/courses";
+const API_URL = "https://learnhub1-ten.vercel.app//api/courses";
 
 function Courses() {
     const [courses, setCourses] = useState([]);
